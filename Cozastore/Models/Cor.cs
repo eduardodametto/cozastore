764347f89c10ccbcbd0d52b31f7efd3b104a6e16
EduardoDametto <eduardodametto@etec.sp.gov.br>
2024-02-20 09:39:45 -0300

    Criado o projeto e a classe Cor

---
+++ Cozastore/Models/Cor.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cozastore.Models;
+
+[Table("Cor")]
+public class Cor
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe o Nome")]
+    [StringLength(30, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
+    public string Nome { get; set; }
+
+    [Display(Name = "Código Hexa", Prompt = "Ex. #000000")]
+    [Required(ErrorMessage = "Por favor, informe o Código Hexa")]
+    [StringLength(7, ErrorMessage = "O Nome deve possuir no máximo 7 caracteres")]
+    public string CodigoHexa { get; set; }
+}
