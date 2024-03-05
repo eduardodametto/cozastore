0ac131f0b6a6ca3e35c79ae5b4bf9792742780c7
EduardoDametto25 <damettoed@gmail.com>
2024-03-05 09:35:44 -0300

    Finalização da camada de dados, criação de contexto e serviço de conexão

---
+++ Cozastore/Models/Usuario.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Cozastore.Models;
+
+[Table("Usuario")]
+public class Usuario
+{
+    [Key]
+    public string UsuarioId { get; set; }
+    [ForeignKey("UsuarioId")]
+    public IdentityUser ContaUsuario { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe o Nome")]
+    [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
+    public string Nome { get; set; }
+
+    [DataType(DataType.Date)]
+    [Display(Name = "Data de Nascimento")]
+    [Required(ErrorMessage = "Por favor, informe o a Data de Nascimento")]
+    public DateTime DataNascimento { get; set; }
+
+    [StringLength(300)]
+    public string Foto { get; set; }
+}
