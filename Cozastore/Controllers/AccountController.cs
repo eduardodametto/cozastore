89a92427695fae1a0141daea3f4e844c322cbad3
EduardoDametto25 <damettoed@gmail.com>
2024-04-23 09:35:45 -0300

    Finalização do login e inclusão dos serviços de autorização e autenticação

--- Cozastore/Controllers/AccountController.cs
+++ Cozastore/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Net.Mail;
 using Cozastore.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -35,7 +36,34 @@ public class AccountController : Controller
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Login(LoginVM login)
     {
-        
+        if (ModelState.IsValid)
+        {
+            string userName = login.Email;
+            if (IsValidEmail(userName))
+            {
+                var user = await _userManager.FindByEmailAsync(userName);
+                if (user != null)
+                    userName = user.UserName;
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {userName} acessou o sistema!");
+                return LocalRedirect(login.UrlRetorno);
+            }
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {userName} está bloqueado");
+                ModelState.AddModelError(string.Empty, "Conta Bloqueada! Aguarde alguns minutos para continuar!");
+            }
+
+            ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!! ");
+        }
         return View(login);
     }
 
@@ -43,5 +71,18 @@ public class AccountController : Controller
     public IActionResult Error()
     {
         return View("Error!");
+    }
+
+    private static bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
